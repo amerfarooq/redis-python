e270e69e58cd0f62761a84e86c8dd7a7ad0e3dad
Amer Farooq <amerfarooq@live.com>
2023-01-15 22:08:52 +0500

    respond to multiple PINGS

--- app/main.py
+++ app/main.py
@@ -2,9 +2,6 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client_socket, client_addr = server_socket.accept()
 
